9fa1e2d025570f453d0c542d01c1d1ffc38b750e
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-09 11:34:17 -0500

    first commit

---
+++ src/test/java/actionTest1.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class actionTest1 {
+    @Test
+    public void test1(){
+        fail("not yet implemented");
+    }
+}
